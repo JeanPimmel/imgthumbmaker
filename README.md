6c789f6be123d1c36ae620d36ecdc20015e8cf04
Jean Pimmel <jeanpimmel@hotmail.com>
2021-06-24 11:00:16 -0300

    in progress

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # imgthumbmaker
 thumbnail little project v1
+
+# how to use
+- python thumb.py {images folder} {the name you want the thumbnail folder to have}
+- (python thumb.py images/ thumb_images/)
+
+# ideas to next versions
+- web page (js)
+- receive uploaded images
--- thumb.py
+++ thumb.py
@@ -11,5 +11,8 @@ if not os.path.exists(output_folder):
 for filename in os.listdir(image_folder):
     img = Image.open(f'{image_folder}{filename}')
     img.thumbnail((200, 400))
+    """ if you want the image to be converted to png
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png', 'png') """
     img.save(f'{output_folder}{filename}')
     print('all done!')
